2d10332c7253470e290ecdc8b513c57da3f7291b
darrenjon <darrenlin23@gmail.com>
2023-05-22 16:41:28 +0800

    feat: add playRound function declares results

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’
+//Computer randomly chooses rock, paper, or scissors
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
@@ -12,4 +12,31 @@ const getComputerChoice = () => {
     }
 }
 
-console.log(getComputerChoice());
+//Player chooses rock, paper, or scissors
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return 'Tie';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You Lose! Paper beats Rock';
+        } else {
+            return 'You Win! Rock beats Scissors';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return 'You Lose! Scissors beats Paper';
+        } else {
+            return 'You Win! Paper beats Rock';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return 'You Lose! Rock beats Scissors';
+        } else {
+            return 'You Win! Scissors beats Paper';
+        }
+    }
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
